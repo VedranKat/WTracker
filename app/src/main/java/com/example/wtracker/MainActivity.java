4d05cc629483079c6e8fc914d06399e594452ecd
unknown <vedstandard@runbox.com>
2022-04-30 15:18:26 +0200

    Botuni, datum, sql

--- app/src/main/java/com/example/wtracker/MainActivity.java
+++ app/src/main/java/com/example/wtracker/MainActivity.java
@@ -4,25 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView weekAndYear;
+    SQLiteDatabase myDb;
+    LocalDate ld;
+    TemporalField woy;
+    int weekNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase myDb = this.openOrCreateDatabase("Vaga", MODE_PRIVATE, null);
+        weekAndYear = findViewById(R.id.textView_WeekYear);
+
+        ld = LocalDate.now();
+        woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+        weekNumber = ld.get(woy);
+
+        myDb = this.openOrCreateDatabase("Vaga", MODE_PRIVATE, null);
+        myDb.execSQL("CREATE TABLE IF NOT EXISTS weeks(week TINYINT, " +
+                "year SMALLINT, monday decimal, tuesday decimal, wednesday decimal," +
+                " thursday decimal, friday decimal, saturday decimal, sunday decimal, average decimal)");
 
 
-        Calendar calendar = Calendar.getInstance();
-        int week = calendar.get(Calendar.WEEK_OF_YEAR);
+
+        Log.d("Dayz", String.valueOf(weekNumber));
+        Log.d("Day", String.valueOf(ld.getDayOfWeek().getValue()));
 
 
     }
