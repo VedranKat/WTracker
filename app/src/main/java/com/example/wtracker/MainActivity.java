a2b86d716787c952661f6aa016dd497cab06c779
unknown <vedstandard@runbox.com>
2022-05-01 02:04:41 +0200

    onclick

--- app/src/main/java/com/example/wtracker/MainActivity.java
+++ app/src/main/java/com/example/wtracker/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.wtracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.time.LocalDate;
@@ -15,18 +21,39 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView weekAndYear;
     SQLiteDatabase myDb;
     LocalDate ld;
     TemporalField woy;
     int weekNumber;
+    Button monday, tuesday, wednesday, thursday, friday, saturday, sunday, back, forward;
+    int weightToDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        monday = findViewById(R.id.buttonMonday);
+        monday.setOnClickListener(this::onClick);
+        tuesday = findViewById(R.id.buttonTuesday);
+        tuesday.setOnClickListener(this::onClick);
+        wednesday = findViewById(R.id.buttonWednesday);
+        wednesday.setOnClickListener(this::onClick);
+        thursday = findViewById(R.id.buttonThursday);
+        thursday.setOnClickListener(this::onClick);
+        friday = findViewById(R.id.buttonFriday);
+        friday.setOnClickListener(this::onClick);
+        saturday = findViewById(R.id.buttonSaturday);
+        saturday.setOnClickListener(this::onClick);
+        sunday = findViewById(R.id.buttonSunday);
+        sunday.setOnClickListener(this::onClick);
+        back = findViewById(R.id.buttonBack);
+        back.setOnClickListener(this::onClick);
+        forward = findViewById(R.id.buttonForward);
+        forward.setOnClickListener(this::onClick);
 
         weekAndYear = findViewById(R.id.textView_WeekYear);
 
@@ -40,10 +67,67 @@ public class MainActivity extends AppCompatActivity {
                 " thursday decimal, friday decimal, saturday decimal, sunday decimal, average decimal)");
 
 
+    }
 
-        Log.d("Dayz", String.valueOf(weekNumber));
-        Log.d("Day", String.valueOf(ld.getDayOfWeek().getValue()));
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonMonday:
+                alert("monday");
+                break;
+            case R.id.buttonTuesday:
+                alert("tuesday");
+                break;
+            case R.id.buttonWednesday:
+                alert("wednesday");
+                break;
+            case R.id.buttonThursday:
+                alert("thursday");
+                break;
+            case R.id.buttonFriday:
+                alert("friday");
+                break;
+            case R.id.buttonSaturday:
+                alert("saturday");
+                break;
+            case R.id.buttonSunday:
+                alert("sunday");
+                break;
+            case R.id.buttonBack:
+                // do your code
+                break;
+            case R.id.buttonForward:
+                // do your code
+                break;
+            default:
+                break;
+        }
+    }
 
+    public void alert(String day){
+        AlertDialog.Builder myDia = new AlertDialog.Builder(MainActivity.this);
+        myDia.setTitle("Body weight in Kilograms?");
+
+        final EditText weightInput = new EditText(MainActivity.this);
+        weightInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        myDia.setView(weightInput);
+
+        myDia.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                weightToDatabase = Integer.parseInt(weightInput.getText().toString());
+            }
+        });
+
+        myDia.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        myDia.show();
 
     }
+
 }
